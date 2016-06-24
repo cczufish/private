6512f8bbff615427ae99a9f960ffe237009b4652
bjmayor <415074476@qq.com>
2016-06-24 11:48:54 +0800

    wifikey

---
+++ wifikey/App/Conf/config.php
@@ -0,0 +1,16 @@
+<?php
+//注意，请不要在这里配置SAE的数据库，配置你本地的数据库就可以了。
+return array(
+        //'配置项'=>'配置值'
+        'SHOW_PAGE_TRACE'=>false,
+        'URL_HTML_SUFFIX'=>'.html',
+        // 添加数据库配置信息
+        'DB_TYPE'   => 'mysql', // 数据库类型
+        'DB_HOST'   => 'localhost', // 服务器地址
+        'DB_NAME'   => 'app_wifikey', // 数据库名
+        'DB_USER'   => 'root', // 用户名
+        'DB_PWD'    => '', // 密码
+        'DB_PORT'   => 3306, // 端口
+        'DB_PREFIX' => 'think_', // 数据库表前缀
+        );
+?>
