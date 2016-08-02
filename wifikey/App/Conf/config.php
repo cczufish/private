7aefcce7c5fbe2ee9f64cc2d543bd087c8cfdba6
bjmayor <415074476@qq.com>
2016-08-02 23:45:43 +0800

    pwd

--- wifikey/App/Conf/config.php
+++ wifikey/App/Conf/config.php
@@ -7,10 +7,10 @@ return array(
         // 添加数据库配置信息
         'DB_TYPE'   => 'mysql', // 数据库类型
         'DB_HOST'   => 'localhost', // 服务器地址
-        'DB_NAME'   => 'app_wifikey', // 数据库名
+        'DB_NAME'   => 'wifikey', // 数据库名
         'DB_USER'   => 'root', // 用户名
-        'DB_PWD'    => '', // 密码
+        'DB_PWD'    => 'YLYP1JvT9KqL', // 密码
         'DB_PORT'   => 3306, // 端口
-        'DB_PREFIX' => 'think_', // 数据库表前缀
+        'DB_PREFIX' => '', // 数据库表前缀
         );
 ?>
